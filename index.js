739d99b51c592a7b252a412feda45271a3731cef
Garret <gwiats@gmail.com>
2021-04-10 18:45:53 -0400

    refined fetch call and added initialRender

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetchDrinks()
+    fetchDrinks(initialRender)
 })
 
-function fetchDrinks(action) {
-    fetch("https://wft-geo-db.p.rapidapi.com/v1/geo/cities?countryIds=US&minPopulation=100000", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "4010cdd83cmsh8cb2b7ba6ad0987p1d44aejsn8d7efd02cd68",
-		"x-rapidapi-host": "wft-geo-db.p.rapidapi.com"
-	}
+function fetchDrinks(action) { //fetchs from api and then executes the desired function on the returned data
+    fetch("https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&countryIds=US&minPopulation=1490000&types=CITY", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "4010cdd83cmsh8cb2b7ba6ad0987p1d44aejsn8d7efd02cd68",
+            "x-rapidapi-host": "wft-geo-db.p.rapidapi.com"
+        }
     })
     .then(res => res.json())
-    .then(data => action(data))
+    .then(res => action(res.data))
     .catch(err => {
 	    console.error(err)
     })
 }
 
+function initialRender(cities) {
+    //console.log(cities)
+    cities.forEach(city => {
+        let adding = document.createElement('li')
+        adding.textContent = city.name
+        adding.id = city.name
+        document.getElementById('city names').appendChild(adding)
+    })
+}
