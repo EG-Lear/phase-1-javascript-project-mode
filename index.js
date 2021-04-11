25f885428880ce835296d2369b7e7dcd60242b17
Garret <gwiats@gmail.com>
2021-04-10 23:17:04 -0400

    small modification to HTML, added most of code to moreInfo

--- index.js
+++ index.js
@@ -42,6 +42,28 @@ function initialRender(cities) { //does the initial render of cities to page
 }
 
 function moreInfo(res) {
-    console.log(res)
+    const loc = res.data
+    document.getElementById('city names').innerHTML = ''
+    let adding = document.createElement('p')
+    adding.textContent = `The city of ${loc.city} is in the state of ${res.data.region}.  It sits ${loc.elevationMeters} above sea level at ${loc.longitude} longitude and ${loc.latitude}.  It has a population of ${loc.population}.`
+    document.getElementById('cityInfo').appendChild(adding)
 }
 
+// city: "Brooklyn"
+// country: "United States of America"
+// countryCode: "US"
+// deleted: false
+// distance: null
+// elevationMeters: 15
+// id: 122111
+// latitude: 40.692777777
+// longitude: -73.990277777
+// name: "Brooklyn"
+// population: 2636735
+// region: "New York"
+// regionCode: "NY"
+// timezone: "America__New_York"
+// type: "CITY"
+// wikiDataId: "Q18419"
+
+//`The city of ${loc.city} is in the state of ${res.data.region}.  It is sits ${loc.elevationMeters} above sea level at ${loc.longitude} longitude and ${loc.latitude}.  It has a population of ${loc.population}.
