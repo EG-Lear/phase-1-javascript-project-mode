d73b59f4253edb86c53586129bf5fd385bbde896
Garret <gwiats@gmail.com>
2021-04-10 21:28:40 -0400

    add fetch request for further data on a single city

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetchDrinks(initialRender)
+    fetchCities(initialRender)
 })
 
-function fetchDrinks(action) { //fetchs from api and then executes the desired function on the returned data
-    fetch("https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&countryIds=US&minPopulation=1490000&types=CITY", {
-        "method": "GET",
-        "headers": {
+const baseUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
+
+function fetchCities(action) { //fetchs from api and then executes the desired function on the returned data
+    fetch(baseUrl + "?limit=10&countryIds=US&minPopulation=1490000&types=CITY", {
+        "method": "GET",  
+        "headers": { 
             "x-rapidapi-key": "4010cdd83cmsh8cb2b7ba6ad0987p1d44aejsn8d7efd02cd68",
             "x-rapidapi-host": "wft-geo-db.p.rapidapi.com"
         }
     })
     .then(res => res.json())
     .then(res => action(res.data))
-    .catch(err => {
-	    console.error(err)
-    })
+    .catch(err => console.error(err))
 }
 
-function initialRender(cities) {
-    //console.log(cities)
+function fetchSingle(id) {
+    fetch(baseUrl + `/${id}`, {
+        "method": "GET",  
+        "headers": { 
+            "x-rapidapi-key": "4010cdd83cmsh8cb2b7ba6ad0987p1d44aejsn8d7efd02cd68",
+            "x-rapidapi-host": "wft-geo-db.p.rapidapi.com"
+        }
+    })
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+}
+
+function initialRender(cities) { //does the initial render of cities to page
+    console.log(cities)
     cities.forEach(city => {
         let adding = document.createElement('li')
         adding.textContent = city.name
-        adding.id = city.name
+        adding.id = city.id
         document.getElementById('city names').appendChild(adding)
     })
 }
+
+// https://wft-geo-db.p.rapidapi.com/v1/geo/cities/
+// 4010cdd83cmsh8cb2b7ba6ad0987p1d44aejsn8d7efd02cd68
