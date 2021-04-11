5b828d71498a27a095de076db9711327ace472f1
Garret <gwiats@gmail.com>
2021-04-10 23:49:49 -0400

    added back button, completed all of planned interactivity

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetchCities(initialRender)
+    document.getElementById('backBtn').disabled = true
+    document.getElementById('backBtn').addEventListener('click', goBack)
 })
 
 const baseUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
+
 
 function fetchCities(action) { //fetchs from api and then executes the desired function on the returned data
     fetch(baseUrl + "?limit=10&countryIds=US&minPopulation=1490000&types=CITY", {
@@ -36,17 +39,35 @@ function initialRender(cities) { //does the initial render of cities to page
         let adding = document.createElement('li')
         adding.textContent = city.name
         adding.id = city.id
-        document.getElementById('city names').appendChild(adding)
+        document.getElementById('cityNames').appendChild(adding)
         document.getElementById(`${city.id}`).addEventListener('click', fetchSingle)
     })
 }
 
 function moreInfo(res) {
     const loc = res.data
-    document.getElementById('city names').innerHTML = ''
+    document.getElementById('cityNames').innerHTML = ''
     let adding = document.createElement('p')
-    adding.textContent = `The city of ${loc.city} is in the state of ${res.data.region}.  It sits ${loc.elevationMeters} above sea level at ${loc.longitude} longitude and ${loc.latitude}.  It has a population of ${loc.population}.`
+    let cLevel = seaLevel(loc.elevationMeters)
+    adding.textContent = `The city of ${loc.city} is in the state of ${res.data.region}.  It sits ${cLevel} sea level at ${loc.longitude} longitude and ${loc.latitude} latitude.  It has a population of ${loc.population} people.`
     document.getElementById('cityInfo').appendChild(adding)
+    document.getElementById('backBtn').disabled = false
+}
+
+function seaLevel(level) {
+    if (level > 0) {
+        return `${level} meters above`
+    } else if (level < 0) {
+        return `${level} meters below`
+    } else {
+        return `directly at`
+    }
+}
+
+function goBack() {
+    document.getElementById('cityInfo').innerHTML = ''
+    fetchCities(initialRender)
+    document.getElementById('backBtn').disabled = true
 }
 
 // city: "Brooklyn"
